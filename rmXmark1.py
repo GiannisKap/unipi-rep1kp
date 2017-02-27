5a70f6cede0bf4ca3c34add90287eb553e4150d7
GiannisKap <g.kapolosss@gmail.com>
2017-02-27 16:49:36 +0200

    Add files via upload

---
+++ rmXmark1.py
@@ -0,0 +1,29 @@
+e=[]
+d=[]
+cnt=0
+cnt2=0
+strt=0
+sentence=raw_input("Please enter a Sentence:\n")
+sentence=list(sentence)
+for i in sentence:
+    cnt+=1
+for i in range (cnt):
+    if sentence[i]==" " and (sentence[i+1]==sentence[i+1].upper()):
+        d.append(sentence[strt:i])
+        strt=i+1
+d.append(sentence[strt:cnt])
+for i in d:
+    cnt2+=1
+for i in range (cnt2):
+    d[i]="".join(d[i])
+for i in range (cnt2):
+    flag=False
+    for j in range (len(d[i])):
+        if d[i][-1]=="!":
+            flag=True
+        if d[i][j]!="!":
+            e.extend([d[i][j]])
+    if flag==True:
+        e.extend("!")
+    e.extend(" ")
+print "".join(e)
